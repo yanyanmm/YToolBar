fc3032f051f9e06b70a3c37078a23ed972782b94
yanyanmm <singlegirlllj@126.com>
2019-05-27 00:53:11 +0800

    update toolbar icon

--- YToolBarLibrary/src/main/java/com/lihg/library/toolbar/YToolBar.java
+++ YToolBarLibrary/src/main/java/com/lihg/library/toolbar/YToolBar.java
@@ -6,7 +6,10 @@ import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -15,9 +18,14 @@ public class YToolBar extends FrameLayout {
     private Context mContext;
 
     private int mToolBarHeight;
+    private int mPadding;
+
+    private LinearLayout mLeftLayout;
+    private LinearLayout mMiddleLayout;
+    private LinearLayout mRightLayout;
 
     private TextView mTitleView;
-    private LinearLayout mToolBarLayout;
+    private ImageButton mBackBtn;
 
     public YToolBar(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
@@ -27,9 +35,12 @@ public class YToolBar extends FrameLayout {
         super(context, attrs, defStyleAttr);
         mContext = context.getApplicationContext();
         mToolBarHeight = YDesityUtil.dp2px(mContext, 45);
+        mPadding = YDesityUtil.dp2px(mContext, 15);
 
         this.initAttrs(attrs);
         this.initViews();
+
+        this.addBackButton();
     }
 
     private void initAttrs(AttributeSet attrs) {
@@ -51,17 +62,111 @@ public class YToolBar extends FrameLayout {
         mTitleView.setTextColor(Color.BLACK);
         this.addView(mTitleView);
 
-        mToolBarLayout = new LinearLayout(mContext);
-        mToolBarLayout.setLayoutParams(new LayoutParams(-1, -1));
-        this.addView(mToolBarLayout);
-
         View lineView = new View(mContext);
         lineView.setLayoutParams(new LayoutParams(-1, 1, Gravity.BOTTOM));
         lineView.setBackgroundColor(Color.LTGRAY);
         this.addView(lineView);
+
+        LinearLayout layout = new LinearLayout(mContext);
+        layout.setLayoutParams(new LayoutParams(-1, -1));
+        layout.setGravity(Gravity.CENTER_VERTICAL);
+        this.addView(layout);
+
+        mLeftLayout = new LinearLayout(mContext);
+        mLeftLayout.setLayoutParams(new LinearLayout.LayoutParams(-2, -2));
+        mLeftLayout.setGravity(Gravity.CENTER_VERTICAL);
+        layout.addView(mLeftLayout);
+
+        mMiddleLayout = new LinearLayout(mContext);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(-2, -2, 1);
+        params.setMarginStart(mPadding);
+        params.setMarginEnd(mPadding);
+        mMiddleLayout.setLayoutParams(params);
+        mMiddleLayout.setGravity(Gravity.CENTER_VERTICAL);
+        layout.addView(mMiddleLayout);
+
+        mRightLayout = new LinearLayout(mContext);
+        mRightLayout.setLayoutParams(new LinearLayout.LayoutParams(-2, -2));
+        mRightLayout.setGravity(Gravity.CENTER_VERTICAL);
+        layout.addView(mRightLayout);
+    }
+
+    private void addBackButton() {
+        mBackBtn = createButtonWithImage(R.mipmap.back);
+        this.addLeftView(mBackBtn);
+    }
+
+    public void addLeftView(View view) {
+        this.addLeftView(view, mPadding, 0);
+    }
+
+    public void addLeftView(View view, int marginStart, int marginEnd) {
+        if (view != null) {
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(-2, -2);
+            params.setMarginStart(marginStart);
+            params.setMarginEnd(marginEnd);
+            view.setLayoutParams(params);
+            mLeftLayout.addView(view);
+        }
+    }
+
+    public void addMiddleView(View view) {
+        if (view != null) {
+            mMiddleLayout.addView(view);
+        }
+    }
+
+    public void addRightView(View view) {
+        this.addRightView(view, 0, mPadding);
+    }
+
+    public void addRightView(View view, int marginStart, int marginEnd) {
+        if (view != null) {
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(-2, -2);
+            params.setMarginStart(marginStart);
+            params.setMarginEnd(marginEnd);
+            view.setLayoutParams(params);
+            mRightLayout.addView(view);
+        }
+    }
+
+    public ImageButton createButtonWithImage(int resid) {
+        ImageButton btn = new ImageButton(mContext);
+        btn.setBackgroundColor(Color.TRANSPARENT);
+        btn.setImageResource(resid);
+        return btn;
+    }
+
+    public Button createButtonWithText(int resid) {
+        Button btn = new Button(mContext);
+        btn.setBackgroundColor(Color.TRANSPARENT);
+        btn.setText(resid);
+        btn.setTextSize(15);
+        return btn;
+    }
+
+    public Button createButtonWithText(String text) {
+        Button btn = new Button(mContext);
+        btn.setBackgroundColor(Color.TRANSPARENT);
+        btn.setText(text);
+        btn.setTextSize(18);
+        btn.setTextColor(Color.BLACK);
+        return btn;
     }
 
     public TextView getTitleView() {
         return mTitleView;
+    }
+
+    public ImageButton getBackBtn() {
+        return mBackBtn;
+    }
+
+    public int getPadding() {
+        return mPadding;
+    }
+
+    public void setPadding(int padding) {
+        this.mPadding = mPadding;
     }
 }
--- app/src/main/java/com/lihg/toolbar/MainActivity.java
+++ app/src/main/java/com/lihg/toolbar/MainActivity.java
@@ -5,7 +5,10 @@ import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.lihg.library.toolbar.YDesityUtil;
 import com.lihg.library.toolbar.YToolBar;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +24,55 @@ public class MainActivity extends AppCompatActivity {
 
         YToolBar toolBar = findViewById(R.id.toolBar);
         toolBar.getTitleView().setText("班级圈");
-        toolBar.getTitleView().setTextSize(18);
+        toolBar.getBackBtn().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "点击返回", Toast.LENGTH_LONG).show();
+            }
+        });
+        ImageButton rightAddBtn = toolBar.createButtonWithImage(R.mipmap.btn_add);
+        rightAddBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "点击+", Toast.LENGTH_LONG).show();
+            }
+        });
+        toolBar.addRightView(rightAddBtn);
+        ImageButton rightBtn = toolBar.createButtonWithImage(R.mipmap.btn_switch);
+        rightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "点击切换", Toast.LENGTH_LONG).show();
+            }
+        });
+        toolBar.addRightView(rightBtn);
+
+        YToolBar toolBar2 = findViewById(R.id.toolBar2);
+        toolBar2.getTitleView().setVisibility(View.GONE);
+        //toolBar2.getBackBtn().setVisibility(View.GONE);
+        toolBar2.getBackBtn().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "点击返回", Toast.LENGTH_LONG).show();
+            }
+        });
+        int left = 0 - YDesityUtil.dp2px(this, 10);
+        toolBar2.addLeftView(toolBar2.createButtonWithText("班级圈"), left, 0);
+        ImageButton rightAddBtn2 = toolBar2.createButtonWithImage(R.mipmap.btn_add);
+        rightAddBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "点击+", Toast.LENGTH_LONG).show();
+            }
+        });
+        toolBar2.addRightView(rightAddBtn2);
+        ImageButton rightBtn2 = toolBar2.createButtonWithImage(R.mipmap.btn_switch);
+        rightBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "点击切换", Toast.LENGTH_LONG).show();
+            }
+        });
+        toolBar2.addRightView(rightBtn2);
     }
 }
